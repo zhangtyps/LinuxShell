af1224b48fe94e46e2aea76450bf2ceb63da56ce
zhangtianyu <zhangtyps@vip.qq.com>
2018-11-16 11:53:47 +0800

    2018-11-16

---
+++ count_squidlog.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#根据squid请求日志计算代理请求成功率
+Filename=`find / -name access.log`
+echo "------------------------------------------------------------------------------------------"
+Num=`cat $Filename |wc -l`
+echo "代理请求总量：" $Num
+
+Num200=`cat $Filename | grep "/200" | wc -l`
+Num301=`cat $Filename | grep "/301" | wc -l`
+Num302=`cat $Filename | grep "/302" | wc -l`
+Num304=`cat $Filename | grep "/304" | wc -l`
+Numsuccess=$[$Num304+$Num302+$Num301+$Num200]
+Successpre=`awk 'BEGIN{printf "%.1f%%\n",('$Numsuccess'/'$Num')*100}'`
+echo "代理请求成功率：" $Successpre
+
+echo "代理请求url排名前10统计："
+cat  $Filename |awk '{print  $7}' |sort -n |uniq -c | sort -n | tail -10 | awk '{print $1,$2}' |sort -hr
+
+:<<!
+echo "请求方式总量："
+cat $Filename |awk '{print  $6}' |sort -r |uniq -c |sort -n |awk '{print $1,$2}' |sort -hr
+
+echo "请求状态码结果："
+cat $Filename  |awk '{print $4}' |sort -n |uniq -c | awk '{print $1,$2}' |sort -hr
+!
+echo "------------------------------------------------------------------------------------------"
---
+++ loop_mission.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#简单的循环执行命令，此为10秒执行一次，在定时任务里1分钟启动一次
+COUNT=0
+while true
+do
+let COUNT++
+echo "循环 $COUNT"
+svn up /data1/www/changan_dev/
+if [ $COUNT -eq 6 ];then
+echo "程序退出"
+exit 0
+fi
+sleep 10
+done
