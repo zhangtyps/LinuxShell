3fd1669d17fa78d5f31ea10093e69431667b19a6
zhangtianyu <zhangtyps@vip.qq.com>
2018-12-04 15:56:41 +0800

    修改了一些注释内容

--- scp_autodown.sh
+++ scp_autodown.sh
@@ -2,7 +2,7 @@
 #自动通过scp命令下载远程主机上的文件，无需交互式输入密码，一切自动完成
 #需先安装expect，直接yum install expect -y
 
-#要使用参数模式，可以用下面注释内容的写法，注意某些密码含特殊字符，参数使用时要加入引号
+#要使用参数模式，可以用按注释里的写法改写脚本，注意某些密码含特殊字符，参数使用时要加入引号
 #set port [lrange $argv 0 0]
 #set host [lrange $argv 1 1]
 set port "scp主机ssh端口号"
