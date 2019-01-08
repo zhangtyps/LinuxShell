7693b33b007f06eddf5c82597388d2c4aeef80c2
zhangtianyu <zhangtyps@vip.qq.com>
2019-01-08 10:32:05 +0800

    修改了mail.sh相关注释信息

--- curl_alarm/mail.sh
+++ curl_alarm/mail.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# 想要让Linux发送邮件，就必须配置默认的邮件发送参数，编辑/etc/mail.rc配置文件，加入如下内容：
+# set from=发件人邮箱
+# set smtp=smtp服务器
+# set smtp-auth-user=发件人邮箱账户
+# set smtp-auth-password=发件人邮箱账户密码
+# set smtp-auth=login
 to=$1
 subject=$2
 FILE=$3
