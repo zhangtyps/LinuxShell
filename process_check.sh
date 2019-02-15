67e1912722d59df9f4cdb8f25f2ea51b419fe38f
zhangtyps-qb <zhangtyps@vip.qq.com>
2019-02-15 14:10:04 +0800

    修正一些bug，优化了输出结果

--- process_check.sh
+++ process_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #访问我的GitHub获得最新的代码：https://github.com/zhangtyps
 #检测进程监听的状态，自动重启挂了的端口监听进程
-#ver1.0
+#ver1.1 修正一些bug，优化了输出结果
 
 #定义需要检查的进程
 process[0]="nginx"
 process[1]="php-fpm"
-process[2]="zabbix"
+process[2]="zabbix_agentd"
 
 function get_time() {
     echo `date +'%Y-%m-%d %H:%M:%S'`
@@ -17,7 +17,7 @@ function check_process() {
     if [ $num -ge 1 ];then
         echo "$1 is running..."
     else
-        service $1 restart
+        service $1 restart &>/dev/null
         if [ $? -eq 0 ];then
             echo "$1 has been restarted successfully!"
         else
