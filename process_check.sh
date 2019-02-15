217789248dcbff5e6f176f0b95749329339a89ac
zhangtyps-qb <zhangtyps@vip.qq.com>
2019-02-15 11:31:12 +0800

    增加了进程监测脚本

---
+++ process_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#访问我的GitHub获得最新的代码：https://github.com/zhangtyps
+#检测进程监听的状态，自动重启挂了的端口监听进程
+#ver1.0
+
+#定义需要检查的进程
+process[0]="nginx"
+process[1]="php-fpm"
+process[2]="zabbix"
+
+function get_time() {
+    echo `date +'%Y-%m-%d %H:%M:%S'`
+}
+
+function check_process() {
+    num=`netstat -ntpl | grep $1 | wc -l`
+    if [ $num -ge 1 ];then
+        echo "$1 is running..."
+    else
+        service $1 restart
+        if [ $? -eq 0 ];then
+            echo "$1 has been restarted successfully!"
+        else
+            echo "$1 restart failed!"
+        fi
+    fi
+}
+
+#main
+i=0
+get_time
+while [ $i -lt ${#process[@]} ]
+do
+    check_process ${process[i]}
+    let i++
+done
