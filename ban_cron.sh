c6f47769a4eb24cb4fab4a4fb4b99edebf103a8c
zhangtyps-qb <zhangtyps@vip.qq.com>
2019-03-14 18:06:18 +0800

    修改了使用说明，解决了普通用户无法使用crontab命令的一个bug

--- ban_cron.sh
+++ ban_cron.sh
@@ -4,14 +4,15 @@
 @Time : 2019/02/27 10:41:31
 @Author : 修改于zhangtyps
 @GitHub : https://github.com/zhangtyps
-@Version : 1.0
+@Version : 1.1
 @Desc : 替换crontab命令，禁止使用-r参数，每次修改后自动备份定时任务，删除的旧备份
 INFO
 
 # 使用方法：
-# 1.在/etc/bashrc中加入这么一行，路径请根据脚本位置自行修改
-#   alias crontab='/root/ban_cron.sh'
-# 2.执行source /etc/bashrc使之生效即可
+# 1.部署脚本，请给此脚本777的权限，否则普通用户无法使用crontab命令（切记！）
+# 2.在/etc/bashrc中加入这么一行，路径请根据脚本位置自行修改（一定要放在普通用户有r-w权限的目录下，原因同第一条）
+#   alias crontab='/mnt/ban_cron.sh'
+# 3.执行source /etc/bashrc使之生效即可
 # ps:测试请先用crontab -e看看是否有备份，再测试crontab -r命令
 
 source /etc/profile;
